1e12f01b75f5dbf51d8e12cea14f58174089cab4
Nguyen Phu han <nguyenphuhan@f88.vn>
2021-04-28 13:44:34 +0700

    BaseApiController

---
+++ DAY-1/Sample/CleanArchitecture/CleanArchitecture/src/CleanArchitecture.WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        public BaseApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public BaseApiController()
+        {
+            if (_mediator == null)
+            {
+                _mediator = HttpContext.RequestServices.GetService<IMediator>();
+            }
+        }
+    }
+}
